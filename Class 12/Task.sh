e52b0422cabb6ad520db15356aef0d31a8c82bd8
Кирилл Миронов <mironov.ka@edu.spbstu.ru>
2023-05-20 10:54:35 +0300

    add Task

---
+++ Class 12/Task.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+passwd= "qwerty"
+SymNum=0
+BigSymNum= 0
+NumbNum= 0
+SpecSymbNum= 0
+
+echo $passwd
+echo $SymNum
+echo $BigSymNum
+echo $NumbNum
+echo $SpecSymbNum
+
+
+# while read -n 1 chr 
+# do
+#     $SymNum= $SymNum + 1
+# 	if [ "$chr" = "#" ]
+# 	then
+# 		break 1
+# 	else
+# 		echo -n $chr >> ./file_without_comments
+# 	fi
+# 	echo -n $chr
+# done < $passwd
+
